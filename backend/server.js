4d467a1590a61952461b67bafc5001435b3d4d45
Zakaria Moshi <zkemoshi@gmail.com>
2022-03-15 19:25:15 +0300

    node version updated pakcage3

--- backend/server.js
+++ backend/server.js
@@ -17,9 +17,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/v1/users', require('./routes/userRoutes'));
 app.use('/api/v1/payments', require('./routes/paymentRoute'));
 
-// Error Middleware
-app.use(errorHandler);
-
 // Server Frontend
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
@@ -30,6 +27,9 @@ if (process.env.NODE_ENV === 'production') {
     )
   );
 }
+
+// Error Middleware
+app.use(errorHandler);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
