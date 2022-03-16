aab0963b3dffc80a738e7824cf61ac47467c3009
Zakaria Moshi <zkemoshi@gmail.com>
2022-03-16 08:01:01 +0300

    full pack email client3

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use('/api/v1/payments', require('./routes/paymentRoute'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-  app.get('/', (req, res) =>
+  app.get('*', (req, res) =>
     res.sendFile(
       path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
     )
